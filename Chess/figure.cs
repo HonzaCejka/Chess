644a310e114a93919c2dcb387f6c0e0c039d7f70
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-03-29 09:13:12 +0200

    Přidejte soubory projektu.

---
+++ Chess/figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    public class figure
+    {
+        public FigureType Type { get; set; }
+
+        public figure()
+        {
+           
+        }
+    }
+
+    public enum FigureType 
+    { 
+        Pawn, //pěšec
+        Rook, //věž
+        Knight, //jezdec
+        Bishop, //střelec
+        Queen, //Dáma
+        King //král
+    }
+}
