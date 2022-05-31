ae616335c270d37bb3a88845e5ebd00fed7abebd
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-05-31 08:20:20 +0200

    life could be dream

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -31,43 +31,7 @@ namespace Chess
             
 
             InitializeComponent();
-            figures.Add(new Figure(FigureType.Rook, "A8", FigureColor.black));
-            figures.Add(new Figure(FigureType.Rook, "H8", FigureColor.black));
-            figures.Add(new Figure(FigureType.Knight, "B8", FigureColor.black));
-            figures.Add(new Figure(FigureType.Knight, "G8", FigureColor.black));
-            figures.Add(new Figure(FigureType.Bishop, "C8", FigureColor.black));
-            figures.Add(new Figure(FigureType.Bishop, "F8", FigureColor.black));
-            figures.Add(new Figure(FigureType.King, "E8", FigureColor.black));
-            figures.Add(new Figure(FigureType.Queen, "D8", FigureColor.black));
-                            
-                            
-            figures.Add(new Figure(FigureType.Pawn, "H7", FigureColor.black));
-            figures.Add(new Figure(FigureType.Pawn, "B7", FigureColor.black));
-            figures.Add(new Figure(FigureType.Pawn, "G7", FigureColor.black));
-            figures.Add(new Figure(FigureType.Pawn, "C7", FigureColor.black));
-            figures.Add(new Figure(FigureType.Pawn, "F7", FigureColor.black));
-            figures.Add(new Figure(FigureType.Pawn, "E7", FigureColor.black));
-            figures.Add(new Figure(FigureType.Pawn, "A7", FigureColor.black));
-            figures.Add(new Figure(FigureType.Pawn, "D7", FigureColor.black));
-                            
-            figures.Add(new Figure(FigureType.Rook, "A1", FigureColor.white));
-            figures.Add(new Figure(FigureType.Rook, "H1", FigureColor.white));
-            figures.Add(new Figure(FigureType.Knight, "B1", FigureColor.white));
-            figures.Add(new Figure(FigureType.Knight, "G1", FigureColor.white));
-            figures.Add(new Figure(FigureType.Bishop, "C1", FigureColor.white));
-            figures.Add(new Figure(FigureType.Bishop, "F1", FigureColor.white));
-            figures.Add(new Figure(FigureType.King, "E1", FigureColor.white));
-            figures.Add(new Figure(FigureType.Queen, "D1", FigureColor.white));
-                            
-            figures.Add(new Figure(FigureType.Pawn, "H2", FigureColor.white));
-            figures.Add(new Figure(FigureType.Pawn, "B2", FigureColor.white));
-            figures.Add(new Figure(FigureType.Pawn, "G2", FigureColor.white));
-            figures.Add(new Figure(FigureType.Pawn, "C2", FigureColor.white));
-            figures.Add(new Figure(FigureType.Pawn, "F2", FigureColor.white));
-            figures.Add(new Figure(FigureType.Pawn, "E2", FigureColor.white));
-            figures.Add(new Figure(FigureType.Pawn, "A2", FigureColor.white));
-            figures.Add(new Figure(FigureType.Pawn, "D2", FigureColor.white));
-
+            FigureList();
             createDictionary();
 
             vypis.Text = "";
@@ -78,6 +42,46 @@ namespace Chess
             DrawBoard();
             DrawFigures(figures);
 
+        }
+
+        private void FigureList()
+        {
+            figures.Add(new Figure(FigureType.Rook, "A8", FigureColor.black));
+            figures.Add(new Figure(FigureType.Rook, "H8", FigureColor.black));
+            figures.Add(new Figure(FigureType.Knight, "B8", FigureColor.black));
+            figures.Add(new Figure(FigureType.Knight, "G8", FigureColor.black));
+            figures.Add(new Figure(FigureType.Bishop, "C8", FigureColor.black));
+            figures.Add(new Figure(FigureType.Bishop, "F8", FigureColor.black));
+            figures.Add(new Figure(FigureType.King, "E8", FigureColor.black));
+            figures.Add(new Figure(FigureType.Queen, "D8", FigureColor.black));
+
+
+            figures.Add(new Figure(FigureType.Pawn, "H7", FigureColor.black));
+            figures.Add(new Figure(FigureType.Pawn, "B7", FigureColor.black));
+            figures.Add(new Figure(FigureType.Pawn, "G7", FigureColor.black));
+            figures.Add(new Figure(FigureType.Pawn, "C7", FigureColor.black));
+            figures.Add(new Figure(FigureType.Pawn, "F7", FigureColor.black));
+            figures.Add(new Figure(FigureType.Pawn, "E7", FigureColor.black));
+            figures.Add(new Figure(FigureType.Pawn, "A7", FigureColor.black));
+            figures.Add(new Figure(FigureType.Pawn, "D7", FigureColor.black));
+
+            figures.Add(new Figure(FigureType.Rook, "A1", FigureColor.white));
+            figures.Add(new Figure(FigureType.Rook, "H1", FigureColor.white));
+            figures.Add(new Figure(FigureType.Knight, "B1", FigureColor.white));
+            figures.Add(new Figure(FigureType.Knight, "G1", FigureColor.white));
+            figures.Add(new Figure(FigureType.Bishop, "C1", FigureColor.white));
+            figures.Add(new Figure(FigureType.Bishop, "F1", FigureColor.white));
+            figures.Add(new Figure(FigureType.King, "E1", FigureColor.white));
+            figures.Add(new Figure(FigureType.Queen, "D1", FigureColor.white));
+
+            figures.Add(new Figure(FigureType.Pawn, "H2", FigureColor.white));
+            figures.Add(new Figure(FigureType.Pawn, "B2", FigureColor.white));
+            figures.Add(new Figure(FigureType.Pawn, "G2", FigureColor.white));
+            figures.Add(new Figure(FigureType.Pawn, "C2", FigureColor.white));
+            figures.Add(new Figure(FigureType.Pawn, "F2", FigureColor.white));
+            figures.Add(new Figure(FigureType.Pawn, "E2", FigureColor.white));
+            figures.Add(new Figure(FigureType.Pawn, "A2", FigureColor.white));
+            figures.Add(new Figure(FigureType.Pawn, "D2", FigureColor.white));
         }
 
         private ImageSource GetImage(byte[] resource)
@@ -108,8 +112,7 @@ namespace Chess
             int indexrow = rows[figure.Position.Substring(1,1)];
             Grid.SetColumn(rectangle, indexcol);
             Grid.SetRow(rectangle,indexrow);
-
-            ChessBoardGrid.Children.Add(rectangle);   
+            ChessBoardGrid.Children.Add(rectangle);
         }
 
         public void createDictionary()
@@ -163,11 +166,11 @@ namespace Chess
                     rectangle.HorizontalAlignment = HorizontalAlignment.Stretch;
                     if ((x+y) % 2 == 0)
                     { 
-                        rectangle.Fill = new SolidColorBrush(Color.FromRgb(255,255,255));                   
+                        rectangle.Fill = new SolidColorBrush(Color.FromRgb(236, 236, 208));                   
                     }
                     else
                     {
-                        rectangle.Fill = new SolidColorBrush(Color.FromRgb(65, 43, 21));
+                        rectangle.Fill = new SolidColorBrush(Color.FromRgb(119, 149, 87));
                     }
                     
                     Grid.SetColumn(rectangle, x);
